2d909f8977ffe12acb1934a60b7287f8a3726989
Taichi Arima <positive703@gmail.com>
2024-11-20 14:17:35 +0900

    `FlashingModifier`を追加

--- Package.swift
+++ Package.swift
@@ -1,21 +1,20 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.10
 
 import PackageDescription
 
 let package = Package(
     name: "Flash",
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Flash",
             targets: ["Flash"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Flash"),
-
+            name: "Flash"
+        ),
     ]
 )
--- Sources/Flash/Flash.swift
+++ Sources/Flash/Flash.swift
@@ -1,2 +1,63 @@
 // The Swift Programming Language
-// https://docs.swift.org/swift-book
+// https://docs.swift.org/swift-
+
+import Combine
+import SwiftUI
+
+/// Flash the view like scrollIndicator flashes
+public struct FlashingModifier: ViewModifier {
+    public let trigger: PassthroughSubject<Void, Never>
+    @State private var isVisible = false
+
+    @State private var currentTask: Task<Void, any Error>?
+
+    public func body(content: Content) -> some View {
+        content
+            .opacity(isVisible ? 1 : 0)
+            .onReceive(trigger) { _ in
+                currentTask?.cancel()
+                currentTask = Task { @MainActor in
+                    isVisible = true
+
+                    if #available(iOS 16.0, *) {
+                        try await Task.sleep(for: .seconds(1))
+                    } else {
+                        try await Task.sleep(nanoseconds: NSEC_PER_SEC * 1)
+                    }
+
+                    withAnimation(.easeOut(duration: 0.3)) {
+                        isVisible = false
+                    }
+                }
+            }
+    }
+}
+
+public extension View {
+    func flashing(trigger: PassthroughSubject<Void, Never>) -> some View {
+        modifier(FlashingModifier(trigger: trigger))
+    }
+}
+
+#Preview {
+    struct ContentView: View {
+        let flashTrigger = PassthroughSubject<Void, Never>()
+
+        var body: some View {
+            VStack {
+                Text("フラッシュするテキスト")
+                    .bold()
+                    .flashing(trigger: flashTrigger)
+
+                Button {
+                    flashTrigger.send()
+                } label: {
+                    Text("フラッシュさせる")
+                        .padding()
+                }
+            }
+        }
+    }
+
+    return ContentView()
+}
